0040ba5350dd0e0e739aff8199eab63d02d3d2d2
George Chearswat <georgechearswat@Georges-MacBook-Air.local>
2015-05-01 00:21:42 -0700

    Set up the card objects. Small Thursday night commit!

---
+++ Trump/Models/Card.swift
@@ -0,0 +1,37 @@
+//
+//  Card.swift
+//  Trump
+//
+//  Created by George Chearswat on 5/1/15.
+//  Copyright (c) 2015 George Chearswat. All rights reserved.
+//
+
+import Foundation
+
+enum Suit {
+    case Spade
+    case Heart
+    case Diamond
+    case Club
+}
+
+enum Rank {
+    case Two
+    case Three
+    case Four
+    case Five
+    case Six
+    case Seven
+    case Eight
+    case Nine
+    case Ten
+    case Jack
+    case Queen
+    case King
+    case Ace
+}
+
+class Card: NSObject {
+    var suit:Suit!
+    var rank:Rank!
+}
---
+++ Trump/Models/Deck.swift
@@ -0,0 +1,13 @@
+//
+//  Deck.swift
+//  Trump
+//
+//  Created by George Chearswat on 5/1/15.
+//  Copyright (c) 2015 George Chearswat. All rights reserved.
+//
+
+import Foundation
+
+class Deck: NSObject {
+    var cards:[Card] = []
+}
